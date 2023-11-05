f8c744bf6040e492b6c86dd1f41c21e1aca1abd7
Mostfa Desoky <56658652+mostfamhmd@users.noreply.github.com>
2023-11-05 20:33:27 +0200

    finsh AppBar HomeView

--- lib/features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,8 +1,6 @@
-import 'package:bookly_store/core/utils/asset_icone.dart';
 import 'package:bookly_store/features/Home/presentation/views/widgets/hom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
